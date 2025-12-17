c8448bb5935ecec7f6d19874480f5603ce91a3a2
bala-amypo <bala@amypo.in>
2025-12-17 04:36:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -5,4 +5,33 @@ import java.time.LocalDate;
 public class StudentEntity{
     private String name;
     privvate int id;
+    private LocalDate date;
+    private float cgpa;
+
+    public StudentEntity(){
+        public StudentEntity(String name, int id, LocalDate date, float cgpa){
+            this.name = name;
+            this.id = id;
+            this.date = date;
+            this.cgpa = cgpa;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+        public int getID(){
+            return id;
+        }
+        public void setID(int ID){
+            this.id=id;
+        }
+        public LocalDate getDate(){
+            return date;
+        }
+        public void setDate(LocalDate date){
+            this.date = date;
+        }
+    }
 }
