34cf7c8ffab1f31d17b87e0b7d3e97bf27a766b5
bala-amypo <bala@amypo.in>
2025-12-18 04:52:41 +0000

    User commit

--- src/main/java/com/example/demo/NewController/NewfileController.java
+++ src/main/java/com/example/demo/NewController/NewfileController.java
@@ -1,40 +1,40 @@
-package com.example.demo.newcontroller;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.entity.Student;
-import com.example.demo.newentity.NewfileEntity;
-import com.example.demo.newservice.NewfileService;
-import java.util.List;
-@RequestMapping("/students")
-public class StudentController { 
-private final NewfileService service;
-public StudentController(NewfileService service) {
-this.service = service;
-    }
+// package com.example.demo.newcontroller;
+// import jakarta.validation.Valid;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import com.example.demo.entity.Student;
+// import com.example.demo.newentity.NewfileEntity;
+// import com.example.demo.newservice.NewfileService;
+// import java.util.List;
+// @RequestMapping("/students")
+// public class StudentController { 
+// private final NewfileService service;
+// public StudentController(NewfileService service) {
+// this.service = service;
+//     }
 
-    @PostMapping
-    public ResponseEntity<Student> createStudent(@Valid @RequestBodyStudent service) {
-        return new ResponseEntity<>(service.saveStudent(student), HttpStatus.CREATED);
-    }
-    @GetMapping
-    public List<NewfileEntity> getall() {
-        return service.getall();
-    }
-    @GetMapping("/{id}")
-    public NewfileEntity getStudent(@PathVariable Long id) {
-        return service.getidval(id);
-    }
-    @PutMapping("/{id}")
-    public Student updateStudent(@PathVariable Long id,
-     @Valid @RequestBody NewfileEntity student) {
-        return service.update(id, student);
-    }
+//     @PostMapping
+//     public ResponseEntity<Student> createStudent(@Valid @RequestBodyStudent service) {
+//         return new ResponseEntity<>(service.saveStudent(student), HttpStatus.CREATED);
+//     }
+//     @GetMapping
+//     public List<NewfileEntity> getall() {
+//         return service.getall();
+//     }
+//     @GetMapping("/{id}")
+//     public NewfileEntity getStudent(@PathVariable Long id) {
+//         return service.getidval(id);
+//     }
+//     @PutMapping("/{id}")
+//     public Student updateStudent(@PathVariable Long id,
+//      @Valid @RequestBody NewfileEntity student) {
+//         return service.update(id, student);
+//     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteStudent(@PathVariable Long id) {
-        service.delete(id);
-    }
-}
+//     @DeleteMapping("/{id}")
+//     @ResponseStatus(HttpStatus.NO_CONTENT)
+//     public void deleteStudent(@PathVariable Long id) {
+//         service.delete(id);
+//     }
+// }
--- src/main/java/com/example/demo/newservice/NewfileService.java
+++ src/main/java/com/example/demo/newservice/NewfileService.java
@@ -1,13 +1,13 @@
-package com.example.demo.newservice;
+// package com.example.demo.newservice;
 
-import java.util.List;
+// import java.util.List;
 
-import com.example.demo.newentity.NewfileEntity;
+// import com.example.demo.newentity.NewfileEntity;
 
-public interface NewfileService {
-    NewfileEntity savedata(NewfileEntity newfile);
-    NewfileEntity getidval(Long id);
-    List<NewfileEntity> getall();
-    NewfileEntity update(Long id, NewfileEntity newfile);
-    void delete(Long id);
-}
+// public interface NewfileService {
+//     NewfileEntity savedata(NewfileEntity newfile);
+//     NewfileEntity getidval(Long id);
+//     List<NewfileEntity> getall();
+//     NewfileEntity update(Long id, NewfileEntity newfile);
+//     void delete(Long id);
+// }
